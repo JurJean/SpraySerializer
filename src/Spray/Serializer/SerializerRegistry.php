3443822a86db9595dab19b9464a2060af3628a4b
Jur Jean <jurjean@gmail.com>
2015-01-29 19:16:08 +0100

    Speed improvement locating serializer

--- src/Spray/Serializer/SerializerRegistry.php
+++ src/Spray/Serializer/SerializerRegistry.php
@@ -6,19 +6,43 @@ use RuntimeException;
 
 class SerializerRegistry implements SerializerRegistryInterface
 {
+    /**
+     * @var array<int,SerializerInterface>
+     */
     private $serializers = array();
     
+    /**
+     * @var array<string,int>
+     */
+    private $found = array();
+    
+    /**
+     * Add a serializer
+     * 
+     * @param SerializerInterface $serializer
+     */
     public function add(SerializerInterface $serializer)
     {
         $this->serializers[] = $serializer;
     }
     
+    /**
+     * Find a serializer for $subject, a class name.
+     * 
+     * @param string $subject
+     * @return SerializerInterface
+     * @throws RuntimeException if no serializer accepts $subject
+     */
     public function find($subject)
     {
-        foreach ($this->serializers as $serializer) {
+        if (isset($this->found[$subject])) {
+            return $this->serializers[$this->found[$subject]];
+        }
+        foreach ($this->serializers as $i => $serializer) {
             if ( ! $serializer->accepts($subject)) {
                 continue;
             }
+            $this->found[$subject] = $i;
             return $serializer;
         }
         throw new RuntimeException(sprintf(
@@ -27,12 +51,22 @@ class SerializerRegistry implements SerializerRegistryInterface
         ));
     }
     
+    /**
+     * Check if a serializer accepts $subject
+     * 
+     * @param string $subject
+     * @return boolean
+     */
     public function contains($subject)
     {
-        foreach ($this->serializers as $serializer) {
+        if (isset($this->found[$subject])) {
+            return true;
+        }
+        foreach ($this->serializers as $i => $serializer) {
             if ( ! $serializer->accepts($subject)) {
                 continue;
             }
+            $this->found[$subject] = $i;
             return true;
         }
         return false;
