8a96f4696366d2ee1dc3b0cde094de2940564596
Jur Balledux <j.balledux@pinknelson.nl>
2016-10-26 13:29:27 +0200

    Fixed serialization of Doctrine proxy objects

--- src/Object/DoctrineProxySerializer.php
+++ src/Object/DoctrineProxySerializer.php
@@ -2,7 +2,24 @@
 
 namespace Spray\Serializer\Object;
 
-final class DoctrineProxySerializer
-{
+use Doctrine\Common\Persistence\Proxy;
+use Spray\Serializer\SerializerInterface;
 
+final class DoctrineProxySerializer implements SerializerInterface
+{
+    public function accepts($subject)
+    {
+        $implements = class_implements($subject);
+        return isset($implements[Proxy::class]);
+    }
+
+    public function serialize($subject, &$data = array(), SerializerInterface $serializer = null)
+    {
+
+    }
+
+    public function deserialize($subject, &$data = array(), SerializerInterface $serializer = null)
+    {
+
+    }
 }
